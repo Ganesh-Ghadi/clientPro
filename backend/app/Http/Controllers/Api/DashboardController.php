f39501e4ea6560cfe1892c10edbede8de4ea85da
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2025-02-21 19:02:39 +0530

    first commit

---
+++ backend/app/Http/Controllers/Api/DashboardController.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Profile;
+use App\Models\Receipt;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Api\BaseController;
+
+class DashboardController extends BaseController
+{
+     /**
+     * Dashboard
+     */
+    public function index(Request $request): JsonResponse
+    {
+        $profileCount = Profile::count();
+
+        $today = now()->toDateString(); 
+        $receiptCountToday = Receipt::whereDate('created_at', $today)->count(); 
+        $totalAmountToday = Receipt::whereDate('created_at', $today)
+        ->where('cancelled',false)
+        ->sum('amount');
+        $cancelledReceipts = Receipt::whereDate('created_at', $today)
+                                    ->where('cancelled', true)
+                                    ->count();
+
+         $date = now()->toDateString(); 
+
+    $receipts = Receipt::with(['poojas.poojaType.devta'])  
+        ->where("cancelled", false)
+        ->whereHas('poojas', function ($query) use ($date) {
+            $query->where('date', $date);
+        })
+        ->get();
+    
+    $poojaDetails = $receipts->flatMap(function ($receipt) use ($date) {
+        return $receipt->poojas->filter(function ($pooja) use ($date) {
+            return $pooja->date == $date;  
+        })->map(function ($pooja) use ($receipt) {
+            return [
+                'name' => $receipt->name,  // User's name from receipt
+                'gotra' => $receipt->gotra,  // User's name from receipt
+                'email' => $receipt->email, // Email from receipt
+                'mobile' => $receipt->mobile, // Mobile from receipt
+                'pooja_type' => $pooja->poojaType->pooja_type,  // Name of the pooja type
+                'devta_name' => $pooja->poojaType->devta->devta_name, // Devta name from the related devta model
+                'date' => $pooja->date,  // Date of the pooja (today's date)
+            ];
+        });
+    });
+
+    $hallReceipts = Receipt::with('hallReceipt')
+                            ->where('receipt_type_id', 9)
+                            ->where('special_date',$today)
+                            ->where("cancelled", false)
+                            ->get();
+    
+    
+    $hallBookingDetails = $hallReceipts->map(function ($receipt) {
+        $hallBooking = $receipt->hallReceipt; // Assuming it's a single model
+        return [
+            'receipt_id' => $receipt->id,
+            'hall_name' => $hallBooking ? $hallBooking->hall : 'N/A', // Fallback if hallReceipt is null
+            'from_time' => $hallBooking && $hallBooking->from_time ? \Carbon\Carbon::parse($hallBooking->from_time)->format('h:i A') : 'N/A',  // Fallback if from_time is null
+            'to_time' => $hallBooking && $hallBooking->to_time ? \Carbon\Carbon::parse($hallBooking->to_time)->format('h:i A') : 'N/A',      // Fallback if to_time is null
+            'amount' => $receipt->amount,
+            'name' => $receipt->name,
+        ];
+    });
+
+    $prasadReceipts = Receipt::where('receipt_type_id', 15)
+                    ->where('special_date',$today)
+                    ->where("cancelled", false)
+                    ->get();
+    
+    $prasadReceiptDetails = $prasadReceipts->map(function ($receipt) {
+            return [
+                'receipt_id' => $receipt->id,
+                'person_name' => $receipt->name ? $receipt->name : "N/A", // Fallback if hallReceipt is null
+                'amount' => $receipt->amount,
+            ];
+    });
+
+    $totalPoojas = $poojaDetails->count();
+    $totalHallBookings = $hallBookingDetails->count();
+    $totalPrasadCount = $prasadReceiptDetails->count();
+
+    $sareeReceipt = Receipt::with('sareeReceipt')
+                            ->where("cancelled", false)
+                            ->whereHas('sareeReceipt', function($query) use ($date) {
+                                $query->where('saree_draping_date_morning',$date);
+                            })
+                            ->first();
+
+                            if ($sareeReceipt) {
+                                $sareeDetails = [
+                                    'saree_draping_date_morning' => $sareeReceipt->sareeReceipt->saree_draping_date_morning,
+                                    'return_saree' => $sareeReceipt->sareeReceipt->return_saree,
+                                    'name' => $sareeReceipt->name,
+                                    'gotra' => $sareeReceipt->gotra,
+                                ];
+                            } else {
+                                // Handle the case where no matching receipt was found
+                                $sareeDetails = null;
+                            }
+
+    $uparaneReceipt = Receipt::with('uparaneReceipt')
+                    ->where("cancelled", false)
+                    ->whereHas('uparaneReceipt', function($query) use ($date) {
+                        $query->where('uparane_draping_date_morning',$date);
+                    })
+                    ->first();
+                    if ($uparaneReceipt) {
+                       $uparaneDetails = [
+                        'name' => $uparaneReceipt->name,
+                        'gotra' => $uparaneReceipt->gotra,
+                    ];
+                } else {
+                    // Handle the case where no matching receipt was found
+                    $uparaneDetails = null;
+            }
+                            
+        return $this->sendResponse(["ProfileCount"=>$profileCount,
+                                'ReceiptCount'=>$receiptCountToday,
+                                'ReceiptAmount'=>$totalAmountToday,
+                                'CancelledReceiptCount'=>$cancelledReceipts,
+                                'PoojaDetails'=>$poojaDetails,
+                                'HallBookingDetails'=>$hallBookingDetails,
+                                'PrasadReceiptDetails'=>$prasadReceiptDetails,
+                                'PoojaCount'=>$totalPoojas,
+                                'HallBookingCount'=>$totalHallBookings,
+                                'PrasadCount'=>$totalPrasadCount,
+                                'SareeDetails'=>$sareeDetails,
+                                'UparaneDetails'=>$uparaneDetails,
+                                ], "Dashboard data retrieved successfully");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+//     public function todaysPooja(): JsonResponse
+//     {
+//         // Get today's date
+//     $date = now()->toDateString();  // Using `now()` instead of Date() for simplicity
+
+//     // Retrieve receipts with their poojas, pooja types, and devta information
+//     $receipts = Receipt::with(['poojas.poojaType.devta'])  // Eager load devta info through poojaType
+//         ->where("cancelled", false)
+//         ->whereHas('poojas', function ($query) use ($date) {
+//             // Filter poojas where the date matches today's date
+//             $query->where('date', $date);
+//         })
+//         ->get();
+    
+//     // Flatten and map over the poojas to get the relevant information
+//     $poojaDetails = $receipts->flatMap(function ($receipt) use ($date) {
+//         // Filter the poojas to match today's date
+//         return $receipt->poojas->filter(function ($pooja) use ($date) {
+//             return $pooja->date == $date;  // Match pooja's date with today's date
+//         })->map(function ($pooja) use ($receipt) {
+//             return [
+//                 'name' => $receipt->name,  // User's name from receipt
+//                 'email' => $receipt->email, // Email from receipt
+//                 'mobile' => $receipt->mobile, // Mobile from receipt
+//                 'poojaType' => $pooja->poojaType->pooja_type,  // Name of the pooja type
+//                 'devtaName' => $pooja->poojaType->devta->devta_name, // Devta name from the related devta model
+//                 'date' => $pooja->date,  // Date of the pooja (today's date)
+//             ];
+//         });
+//     });
+
+//     // Return the list of pooja details as a JSON response
+//     return response()->json($poojaDetails);
+//     return $this->sendResponse(["PoojaDetails"=>$poojaDetails, 'ReceiptCount'=>$receiptCountToday, 'ReceiptAmount'=>$totalAmountToday,'CancelledReceiptCount'=>$cancelledReceipts], "Todays Pooja Details retrieved successfully");
+// }
+
+}
---
+++ backend/app/Models/Receipt.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Pooja;
+use App\Models\Profile;
+use App\Models\Receipt;
+use App\Models\PoojaType;
+use App\Models\CampReceipt;
+use App\Models\HallReceipt;
+use App\Models\KhatReceipt;
+use App\Models\ReceiptType;
+use App\Models\NaralReceipt;
+use App\Models\SareeReceipt;
+use App\Models\BhangarReceipt;
+use App\Models\LibraryReceipt;
+use App\Models\UparaneReceipt;
+use App\Models\AnteshteeReceipt;
+use App\Models\StudyRoomReceipt;
+use App\Models\VasturupeeReceipt;
+use Illuminate\Database\Eloquent\Model;
+
+class Receipt extends Model
+{
+    public static function generateReceiptNumber(): string
+    {
+        // Find the latest profile number for the current month and year
+        $latestNumber = Receipt::where('receipt_no', 'like', date('my') . '%')
+                        ->orderBy('receipt_no', 'DESC')
+                        ->first();
+
+        // Increment the numeric part of the profile number
+        $lastNumber = 1;
+
+        if ($latestNumber) {
+            $lastNumber = intval(substr($latestNumber->receipt_no, 4)) + 1;
+        }
+        return date('my') . str_pad($lastNumber, 5, '0', STR_PAD_LEFT);
+    }
+
+    public function receiptType(){
+        return $this->belongsTo(ReceiptType::class, 'receipt_type_id');
+    }
+
+    public function khatReceipt(){
+        return $this->hasOne(KhatReceipt::class, 'receipt_id');
+    }
+
+    public function naralReceipt(){
+        return $this->hasOne(NaralReceipt::class, 'receipt_id');
+    }
+
+    public function pooja(){
+        return $this->hasOne(Pooja::class, 'receipt_id');
+    }
+
+    public function poojas(){
+        return $this->hasMany(Pooja::class, 'receipt_id');
+    }
+
+    public function bhangarReceipt(){
+        return $this->hasOne(BhangarReceipt::class, 'receipt_id');
+    }
+
+    public function sareeReceipt(){
+        return $this->hasOne(SareeReceipt::class, 'receipt_id');
+    }
+
+    public function uparaneReceipt(){
+        return $this->hasOne(UparaneReceipt::class, 'receipt_id');
+    }
+
+    public function vasturupeeReceipt(){
+        return $this->hasOne(VasturupeeReceipt::class, 'receipt_id');
+    }
+
+    public function campReceipt(){
+        return $this->hasOne(CampReceipt::class, 'receipt_id');
+    }
+
+    public function hallReceipt(){
+        return $this->hasOne(HallReceipt::class, 'receipt_id');
+    }
+
+    public function libraryReceipt(){
+        return $this->hasOne(LibraryReceipt::class, 'receipt_id');
+    }
+
+    public function studyRoomReceipt(){
+        return $this->hasOne(StudyRoomReceipt::class, 'receipt_id');
+    }
+
+    public function anteshteeReceipt(){
+        return $this->hasOne(AnteshteeReceipt::class, 'receipt_id');
+    }
+
+    public function profile(){
+        return $this->belongsTo(Profile::class, 'created_by');
+    }
+}
